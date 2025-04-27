af151b9c40c3f7ce1c5c243bc2f46cf5678d9b28
Ad Heinz <Heinzad@users.noreply.github.com>
2025-04-27 21:40:57 +1200

    etl: extract-transform-load package

---
+++ src/etl/__init__.py
@@ -0,0 +1,15 @@
+#src\etl\__init__.py
+
+"""initialise etl package
+
+ETL: Extract-Transform-Load
+===========================
+
+ETL is a three-step process that assumes files have already been landed: 
+
+0. Land
+1. Extract
+2. Transform 
+3. Load
+
+"""
