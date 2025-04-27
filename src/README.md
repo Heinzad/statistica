f244855f93491469d5e15eedf2d9e9795a90db61
Ad Heinz <Heinzad@users.noreply.github.com>
2025-04-27 21:37:08 +1200

    src: Source Code Package

--- src/README.md
+++
@@ -1,9 +0,0 @@
-<!--src\README.md-->
-
-
-
-Source Code 
-=========== 
-
-This folder contains the source code for the project. 
-
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+#src\__init__.py
+
+"""
+
+
+Source Code 
+=========== 
+
+This package contains the source code for the project.
+
+"""
